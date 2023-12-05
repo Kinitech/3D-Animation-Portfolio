a947d5ca9dde9d93fe2991386ec8b3413b7b2e08
kinitech <byronjarrett14@gmail.com>
2023-12-05 17:17:30 +0000

    PDF window href test

--- src/components/PDFDocument.js
+++ src/components/PDFDocument.js
@@ -1,6 +1,6 @@
 
 function PDFDocument({filename}) {
-    const url = 'https://byron.page/' + filename.split('./')[1]
+    const url = window.location.href + filename.split('./')[1]
     const source = `https://docs.google.com/gview?url=${url}&embedded=true`
     return (
             <iframe title={filename} src={source} width="100%" height="100%"/>
